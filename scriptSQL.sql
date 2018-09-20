0891c24df629c8f9b50aea0865f7f641bbc8c504
Pablo Nunes <cpepablito@gmail.com>
2018-09-20 14:47:41 -0300

    sql mod

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -9,54 +9,44 @@ CREATE TABLE proj.funcionarios(
 	salario			NUMERIC(7,2) NOT NULL,
 	ativo			BOOLEAN NOT NULL,
 	idTag			INTEGER NOT NULL,
-    CONSTRAINT pk_userID PRIMARY KEY(userID),
-    CONSTRAINT fk_codDep FOREIGN KEY(codDep)
-        REFERENCES proj.departamentos(codDep),
-    CONSTRAINT fk_codCargo FOREIGN KEY(codCargo)
-        REFERENCES proj.cargos(codCargo),
-    admissao        TIMESTAMP NOT NULL,
-    demissao        TIMESTAMP
+	CONSTRAINT pk_func_UID PRIMARY KEY(userID),
+  CONSTRAINT fk_func_userID FOREIGN KEY(userID) REFERENCES proj.infoPessoais(cpf),
+  CONSTRAINT fk_func_codDep FOREIGN KEY(codDep) REFERENCES proj.departamentos(codDep),
+  CONSTRAINT fk_func_codCargo FOREIGN KEY(codCargo) REFERENCES proj.cargos(codCargo),
+  admissao        TIMESTAMP NOT NULL,
+  demissao        TIMESTAMP
 )
 
 CREATE TABLE proj.infoPessoais(
-	userId			    INTEGER NOT NULL,
-	nome			    VARCHAR(20),
-	sobreNome		    VARCHAR(35),
-	cpf				    VARCHAR(13),
-	rg				    VARCHAR(10)
+	nome			    VARCHAR(20) NOT NULL,
+	sobreNome		    VARCHAR(35) NOT NULL,
+	cpf				    VARCHAR(13) NOT NULL,
+	rg				    VARCHAR(10) NOT NULL,
 	dataNascimento	    TIMESTAMP,
-    largadouro          VARCHAR(40) NOT NULL,
-    numResidencia       INTEGER NOT NULL,
-    complemento         VARCHAR(15),
-    bairro              VARCHAR(15),
-    cidade              VARCHAR(15) NOT NULL,
-    cep                 VARCHAR(9) NOT NULL,
-    estado          VARCHAR(2) NOT NULL
-    CONSTRAINT fk_userID FOREIGN KEY(userID)
-        REFERENCES proj.funcionarios(userID),
-
-    CONSTRAINT pk_cpf PRIMARY KEY(cpf),
-    CONSTRAINT pk_rg PRIMARY KEY(rg)
+  logradouro          VARCHAR(40) NOT NULL,
+  numResidencia       INTEGER NOT NULL,
+  complemento         VARCHAR(15),
+  bairro              VARCHAR(15),
+  cidade              VARCHAR(15) NOT NULL,
+  cep                 VARCHAR(9) NOT NULL,
+  estado          VARCHAR(2) NOT NULL,
+  CONSTRAINT pk_cpf PRIMARY KEY(cpf)
 )
 
 CREATE TABLE proj.contato(
     userID          INTEGER NOT NULL,
-    CONSTRAINT fk_userID FOREIGN KEY(userID)
-        REFERENCES proj.funcionarios(userID),
     tel             VARCHAR(15),
     cel             VARCHAR(15),
     email           VARCHAR(32),
-    CONSTRAINT fk_userID FOREIGN KEY(userID)
-        REFERENCES proj.funcionarios(userID)
+    CONSTRAINT fk_cont_userID  FOREIGN KEY(userID) REFERENCES proj.infoPessoais(cpf),
+    CONSTRAINT pk_cpf PRIMARY KEY(userID)
 )
 
 CREATE TABLE proj.login(
     password        VARCHAR(15) NOT NULL,
     userID          INTEGER NOT NULL,
-    CONSTRAINT fk_userID FOREIGN KEY(userId),
-        REFERENCES proj.funcionarios(userID),
+    CONSTRAINT fk_login_userID FOREIGN KEY(userID) REFERENCES proj.infoPessoais(cpf),
     CONSTRAINT pk_userID PRIMARY KEY(userID)
-
 )
 
 CREATE TABLE proj.departamentos(
@@ -66,14 +56,9 @@ CREATE TABLE proj.departamentos(
 )
 
 CREATE TABLE proj.cargos(
-    codCargo        INTEGER,
+		codDep          INTEGER,
+		codCargo        INTEGER,
     cargo           VARCHAR(10),
-    CONSTRAINT pk_codCargo PRIMARY KEY(codCargo)
+    CONSTRAINT pk_carg_codCargo PRIMARY KEY (codCargo),
+		CONSTRAINT fk_carg_codDep FOREIGN KEY (codDep) REFERENCES proj.departamentos(codDep)
 )
-
-
-
-
-
-
-
