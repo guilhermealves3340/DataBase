481e9696080eb435ee555691b9af1b5b25703a07
Guilherme Alves <guilherme.alves3340@gmail.com>
2018-09-29 12:58:44 -0300

    modelando as tabelas [AINDA NAO FINALIZADO]

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -1,21 +1,21 @@
 CREATE SCHEMA proj
 
-CREATE TABLE proj.funcionarios(
-	userID			INTEGER,
-	codCargo		INTEGER,
-	estadoCivil		INTEGER NOT NULL,
+CREATE TABLE proj.tb_funcionarios(
+	userID			INTEGER NOT NULL SERIAL,
+	codCargo		INTEGER NOT NULL,
+	estadoCivil		INTEGER NOT NULL,             --??? 
 	cargaHoraria	DOUBLE NOT NULL,
 	salario			NUMERIC(7,2) NOT NULL,
 	ativo			BOOLEAN NOT NULL,
 	idTag			INTEGER NOT NULL,
 	CONSTRAINT pk_func_UID PRIMARY KEY(userID),
-  CONSTRAINT fk_func_userID FOREIGN KEY(userID) REFERENCES proj.infoPessoais(cpf),
-  CONSTRAINT fk_func_codCargo FOREIGN KEY(codCargo) REFERENCES proj.cargos(codCargo),
+  CONSTRAINT fk_func_userID FOREIGN KEY(userID) REFERENCES proj.tb_infoPessoais(cpf),
+  CONSTRAINT fk_func_codCargo FOREIGN KEY(codCargo) REFERENCES proj.tb_cargos(codCargo),
   admissao        TIMESTAMP NOT NULL,
   demissao        TIMESTAMP
 )
 
-CREATE TABLE proj.infoPessoais(
+CREATE TABLE proj.tb_infoPessoais(
 	nome			    VARCHAR(20) NOT NULL,
 	sobreNome		    VARCHAR(35) NOT NULL,
 	cpf				    VARCHAR(13) NOT NULL,
@@ -31,32 +31,32 @@ CREATE TABLE proj.infoPessoais(
   CONSTRAINT pk_cpf PRIMARY KEY(cpf)
 )
 
-CREATE TABLE proj.contato(
+CREATE TABLE proj.tb_contato(
     userID          INTEGER NOT NULL,
     tel             VARCHAR(15),
     cel             VARCHAR(15),
     email           VARCHAR(32),
-    CONSTRAINT fk_cont_userID  FOREIGN KEY(userID) REFERENCES proj.infoPessoais(cpf),
+    CONSTRAINT fk_cont_userID  FOREIGN KEY(userID) REFERENCES proj.tb_infoPessoais(cpf),
     CONSTRAINT pk_cpf PRIMARY KEY(userID)
 )
 
-CREATE TABLE proj.login(
+CREATE TABLE proj.tb_login(
     password        VARCHAR(15) NOT NULL,
     userID          INTEGER NOT NULL,
-    CONSTRAINT fk_login_userID FOREIGN KEY(userID) REFERENCES proj.infoPessoais(cpf),
+    CONSTRAINT fk_login_userID FOREIGN KEY(userID) REFERENCES proj.tb_infoPessoais(cpf),
     CONSTRAINT pk_userID PRIMARY KEY(userID)
 )
 
-CREATE TABLE proj.departamentos(
+CREATE TABLE proj.tb_departamentos(
     codDep          INTEGER,
     departamento    VARCHAR(10),
     CONSTRAINT pk_codDep PRIMARY KEY(codDep)
 )
 
-CREATE TABLE proj.cargos(
-			codDep          INTEGER,
+CREATE TABLE proj.tb_cargos(
+		codDep          INTEGER,
 		codCargo        INTEGER,
     cargo           VARCHAR(10),
     CONSTRAINT pk_carg_codCargo PRIMARY KEY (codCargo),
-		CONSTRAINT fk_carg_codDep FOREIGN KEY (codDep) REFERENCES proj.departamentos(codDep)
+		CONSTRAINT fk_carg_codDep FOREIGN KEY (codDep) REFERENCES proj.tb_departamentos(codDep)
 )
