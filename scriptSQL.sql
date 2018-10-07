b003d10e48105d2fee3e1534f6db7e1bf117e907
Guilherme Alves <guilherme.alves3340@gmail.com>
2018-10-07 17:15:37 -0300

    rasp

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -1,14 +1,14 @@
 CREATE SCHEMA proj
 
 CREATE TABLE proj.tb_funcionarios(
-	userID			INTEGER NOT NULL,
-	codCargo		INTEGER NOT NULL,
-	estadoCivil		INTEGER NOT NULL,             --??? 
-	cargaHoraria	DOUBLE NOT NULL,
-	salario			NUMERIC(7,2) NOT NULL,
-	ativo			BOOLEAN NOT NULL,
-	idTag			INTEGER NOT NULL,
-	CONSTRAINT pk_func_UID PRIMARY KEY(userID),
+	userID		    	INTEGER,
+	codCargo		    INTEGER NOT NULL,
+	estadoCivil		  INTEGER NOT NULL,             --??? 
+	cargaHoraria	  DOUBLE NOT NULL,
+	salario			    NUMERIC(7,2) NOT NULL,
+	ativo			      BOOLEAN NOT NULL,
+	idTag			      INTEGER NOT NULL,
+	CONSTRAINT pk_func_userID PRIMARY KEY(userID),
   --CONSTRAINT fk_func_userID FOREIGN KEY(userID) REFERENCES proj.tb_infoPessoais(cpf),
   --CONSTRAINT fk_func_codCargo FOREIGN KEY(codCargo) REFERENCES proj.tb_cargos(codCargo),
   admissao        TIMESTAMP NOT NULL,
@@ -16,17 +16,17 @@ CREATE TABLE proj.tb_funcionarios(
 );
 
 CREATE TABLE proj.tb_infoPessoais(
-	nome			    VARCHAR(20) NOT NULL,
+	nome			      VARCHAR(20) NOT NULL,
 	sobreNome		    VARCHAR(35) NOT NULL,
-	cpf				    VARCHAR(13) NOT NULL,
-	rg				    VARCHAR(10) NOT NULL,
-	dataNascimento	    TIMESTAMP,
-  logradouro          VARCHAR(40) NOT NULL,
-  numResidencia       INTEGER NOT NULL,
-  complemento         VARCHAR(15),
-  bairro              VARCHAR(15),
-  cidade              VARCHAR(15) NOT NULL,
-  cep                 VARCHAR(9) NOT NULL,
+	cpf				      VARCHAR(13) NOT NULL,
+	rg				      VARCHAR(10) NOT NULL,
+	dataNascimento  TIMESTAMP,
+  logradouro      VARCHAR(40) NOT NULL,
+  numResidencia   INTEGER NOT NULL,
+  complemento     VARCHAR(15),
+  bairro          VARCHAR(15),
+  cidade          VARCHAR(15) NOT NULL,
+  cep             VARCHAR(9) NOT NULL,
   estado          VARCHAR(2) NOT NULL,
   CONSTRAINT pk_cpf PRIMARY KEY(cpf)
 );
@@ -60,3 +60,6 @@ CREATE TABLE proj.tb_cargos(
     CONSTRAINT pk_carg_codCargo PRIMARY KEY (codCargo),
 		CONSTRAINT fk_carg_codDep FOREIGN KEY (codDep) REFERENCES proj.tb_departamentos(codDep)
 );
+
+
+DROP SCHEMA proj;
