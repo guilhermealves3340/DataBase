daaa59395d6a06b012dac1fcb9ed9d2ce15aabb4
Pablo Nunes <cpepablito@gmail.com>
2018-12-20 02:13:09 -0200

    adqw

--- appweb/admin/relatorio.php
+++ appweb/admin/relatorio.php
@@ -22,9 +22,10 @@
                                     Relatório
                                 </div>
                             </div><br>
-                            
+
                             <form action="#">
 
+                              <div id="piechart"></div>
 
                             </form>
                         </div>
@@ -36,3 +37,69 @@
         <?php include 'rodape.php'; ?>
     </body>
 </html>
+<?php
+
+$m = new MongoClient('mongodb://localhost', [
+    'username' => 'abc',
+    'password' => 'abc@123',
+    'db'       => 'abc'
+]);
+
+$id = $_GET['_id']
+
+$cursor = tela.horas.find({_id : $id})
+
+$studentData = array();
+
+foreach($cursor as $row){
+
+   foreach($row->marks as $item){
+    $result = array();
+
+    $result ['nome'] = $item->nome;
+
+    $result ['sobrenome']  = $item->sobrenome;
+
+    $result ['cargaHoraria']     = $item->cargaHoraria;
+
+    $result ['horas_compridas']     = $item->horas_compridas;
+
+    $result ['salario']     = $item->salario;
+
+    $funcData[] = $result;
+  }
+
+  }
+
+  echo json_encode($funcData);
+
+?>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<script type="text/javascript">
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Work', 8],
+  ['Eat', 2],
+  ['TV', 4],
+  ['Gym', 2],
+  ['Sleep', 8]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title':'My Average Day', 'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
+
+
+</script>
