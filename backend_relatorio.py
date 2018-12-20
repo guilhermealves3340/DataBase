911cfaed32acadf83284221e9cdd615c23f5465e
Guilherme Alves <guilherme.alves3340@gmail.com>
2018-12-20 02:09:32 -0200

    .

--- backend_relatorio.py
+++ backend_relatorio.py
@@ -7,6 +7,7 @@ import json
 def difHoras(y,x):
     return y - x
 
+
 # Conect Postgres
 conn = pg.connect("dbname=Engenharia user=postgres password=1997")
 
@@ -17,8 +18,6 @@ def query(sql):
         conn.commit()
         cur.close()
         return rows
-
-rows = query('SELECT * FROM proj.tb_pontos')
 
 # Conect MongoDB
 myclient = MongoClient("mongodb://localhost:27017/")
@@ -37,28 +36,41 @@ def calcHoras(lista):
         p2 = difHoras(datetime.combine(lista[1],lista[5]),datetime.combine(lista[1],lista[4]))
         return p1+p2
 
-# Listando todos os ids da tb pontos
-ids = []
-for i in range(len(rows)):
-        if existe(ids,rows[i][0]) == False:
-                ids.append(rows[i][0])
+while True:
 
-horas = datetime.now()
-horas = horas - horas
-docs = []
+    db = myclient["tesla"]
+    col = db['horas']
+    col.drop()
 
-for _id in ids:
-
-    # Somar as horas de cada id
-    pontos = query('select * from proj.tb_pontos where userID = {}'.format(_id))
-
-    for i in pontos:
-        horas += calcHoras(i)
-
-    info = query('select nome,sobrenome,salario,cargaHoraria from proj.tb_funcionario where userID = {} '.format(_id))
-    print(info[0][0])
-    func = dict(_id=_id,nome=info[0][0],sobrenome=info[0][1],salario=float(info[0][2]),cargaHoraria=float(info[0][3]),horas_compridas=str(horas))
-    docs.append(func)
+    db = myclient["tesla"]
+    col = db['horas']
 
 
-col.insert_many(docs)
+
+    rows = query('SELECT * FROM proj.tb_pontos')
+
+    # Listando todos os ids da tb pontos
+    ids = []
+    for i in range(len(rows)):
+            if existe(ids,rows[i][0]) == False:
+                    ids.append(rows[i][0])
+
+    horas = datetime.now()
+    horas = horas - horas
+    docs = []
+
+    for _id in ids:
+
+        # Somar as horas de cada id
+        pontos = query('select * from proj.tb_pontos where userID = {}'.format(_id))
+
+        for i in pontos:
+            horas += calcHoras(i)
+
+            info = query('select nome,sobrenome,salario,cargaHoraria from proj.tb_funcionario where userID = {} '.format(_id))
+            print(info[0][0])
+            func = dict(_id=_id,nome=info[0][0],sobrenome=info[0][1],salario=float(info[0][2]),cargaHoraria=float(info[0][3]),horas_compridas=str(horas))
+            docs.append(func)
+
+
+    col.insert_many(docs)
